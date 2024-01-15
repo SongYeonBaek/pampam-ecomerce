e3a9fd285714c7ea75b3c192a13cad9ddefea0d3
Hosae0905 <didghtls11@gmail.com>
2024-01-15 09:24:52 +0900

    [Init] 최초 프로젝트 생성

---
+++ src/main/java/com/example/pampam/PampamApplication.java
@@ -0,0 +1,13 @@
+package com.example.pampam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PampamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PampamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pampam/PampamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pampam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PampamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
