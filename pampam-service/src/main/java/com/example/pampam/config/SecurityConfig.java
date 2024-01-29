9c49c7bbfa41c9dc54f55b52183b456b2ef444eb
Hosae0905 <didghtls11@gmail.com>
2024-01-29 17:44:32 +0900

    [Fix] 스프링 시큐리티 복구 - JWT 토큰을 적용하면서 테스트를 위해 지웠던 스프링 시큐리티 설정 파일을 다시 복구

---
+++ pampam-service/src/main/java/com/example/pampam/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.pampam.config;
+
+
+
+import com.example.pampam.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    private final MemberService memberService;
+
+    public SecurityConfig(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        try {
+
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/**").permitAll()
+                    .anyRequest().authenticated();
+            http.formLogin().disable();
+            http.sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            return http.build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
