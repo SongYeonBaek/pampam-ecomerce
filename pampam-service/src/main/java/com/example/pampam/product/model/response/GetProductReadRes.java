80e19da10c698b6d60cf03e5fd8e4d7ccb60b060
Hosae0905 <didghtls11@gmail.com>
2024-02-05 19:50:47 +0900

    [Refactor] 회원, 상품, 결제 관련 기능 리펙터링 - 회원 관련 기능 리펙터링 - 주문 관련 기능 리펙터링 - 상품 관련 기능 리펙터링

--- pampam-service/src/main/java/com/example/pampam/product/model/response/GetProductReadRes.java
+++ pampam-service/src/main/java/com/example/pampam/product/model/response/GetProductReadRes.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Builder
 @Data
@@ -15,13 +16,13 @@ public class GetProductReadRes {
     private Integer price;
     private Integer salePrice;
     private String productInfo;
-    private String filename;
+    private List<String> filename;
     private Long sellerIdx ;
     private Integer peopleCount;
     private Date startAt;
     private Date closeAt;
 
-    public static GetProductReadRes entityToDto(Product product, String filename) {
+    public static GetProductReadRes entityToDto(Product product, List<String> filename) {
         return GetProductReadRes.builder()
                 .idx(product.getIdx())
                 .productName(product.getProductName())
